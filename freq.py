352535c8267b175719e6fb3bcd78da7fab048523
Josh Kosloski <josh@darkfox.io>
2021-02-10 17:26:22 -0500

    Add comments, errorchecking for filename input

--- freq.py
+++ freq.py
@@ -8,18 +8,27 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
-file_name = sys.argv[1]
+#Require file variable to be passed when invoking script
+try:
+    file_name = sys.argv[1]
+except:
+    print("PencilLink freq.py usage:")
+    print("freq.py [filename.csv]")
+    sys.exit(1)
 
 dtypes = {
     " remoteNumber": "string",
     "Frequency": "string",
 }
+
+#Create Pandas DataFrame from CSV file
 df = pd.read_csv(
     file_name,
     dtype=dtypes,
     usecols=[" remoteNumber"]
 )
 
+#Create new column in DataFrame for normalized domestic numbers
 df['phoneNumber'] = df[' remoteNumber'].astype(str).str[-10:]
 
 #Set Variable for Print to Screen
@@ -28,4 +37,5 @@ n_by_number = df.groupby("phoneNumber")["phoneNumber"].count().sort_values(ascen
 #Print to CSV
 df.groupby("phoneNumber")["phoneNumber"].count().sort_values(ascending=False).to_csv('frequency_report.csv')
 
+#Print top 25 to screen
 print(n_by_number.head(25))
